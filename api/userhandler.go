1db041d5e79e885bcd3014caeb9dcfb8cdee8777
Kanistha Acharya <kanistha.acharya@daimler.com>
2020-08-09 21:02:41 +0200

    refact: update func name

--- api/userhandler.go
+++ api/userhandler.go
@@ -9,13 +9,13 @@ import (
 func Handle(writer http.ResponseWriter, request *http.Request) {
 
 	fmt.Println("Handler called")
-	handle("go-service")
+	HandleRequest("go-service")
 	response, _ := json.Marshal("Hello from go-service")
 	writer.WriteHeader(http.StatusOK)
 	writer.Write(response)
 }
 
-func handle(msg string) string {
+func HandleRequest(msg string) string {
 	fmt.Println("User handler called......... ", msg)
 	return "Hello"+msg
 }
